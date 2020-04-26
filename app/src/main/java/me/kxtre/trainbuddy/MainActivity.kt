045aaa4e6e4eb904b1cefbe096e2de7e4f2dd82c
Tiago <tiagodabatalha@hotmail.com>
2020-04-26 18:23:00 +0100

    chore(base): added data binding transformation

--- app/src/main/java/me/kxtre/trainbuddy/MainActivity.kt
+++ app/src/main/java/me/kxtre/trainbuddy/MainActivity.kt
@@ -2,11 +2,13 @@ package me.kxtre.trainbuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import me.kxtre.trainbuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
